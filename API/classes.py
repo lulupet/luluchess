3d734ba731a3af7a6c49457fedfbc2560ac169fb
Lucas Petit <lucas.petit@riminder.net>
2019-10-30 09:04:13 +0100

    feat:filter moves depending on piece type and square availability

--- API/classes.py
+++ API/classes.py
@@ -1,4 +1,4 @@
-from utils import ROWS, COLUMNS, previous_column, next_column, previous_row, next_row, diagonals
+from utils import ROWS, COLUMNS, previous_column, next_column, previous_row, next_row, diagonals, filter_moves
 
 class Board:
 
@@ -48,34 +48,79 @@ class Board:
             Pawn('black', '7', 'H'),
         ]
 
+        self.white_pawns = [pawn for pawn in self.pawns if pawn.color == 'white']
+        self.white_bishops = [bishop for bishop in self.bishops if bishop.color == 'white']
+        self.white_knights = [knight for knight in self.knights if knight.color == 'white']
+        self.white_rooks = [rook for rook in self.rooks if rook.color == 'white']
+        self.white_queens= [queen for queen in self.queens if queen.color == 'white']
+        self.white_king = self.kings[0] if self.kings[0].color == 'white' else self.kings[1] 
+
+        self.white_pieces = self.white_pawns + self.white_bishops + self.white_knights + self.white_rooks + self.white_queens + [self.white_king]
+
+        self.black_pawns = [pawn for pawn in self.pawns if pawn.color == 'black']
+        self.black_bishops = [bishop for bishop in self.bishops if bishop.color == 'black']
+        self.black_knights = [knight for knight in self.knights if knight.color == 'black']
+        self.black_rooks = [rook for rook in self.rooks if rook.color == 'black']
+        self.black_queens= [queen for queen in self.queens if queen.color == 'black']
+        self.black_king = self.kings[0] if self.kings[0].color == 'black' else self.kings[1] 
+
+        self.black_pieces = self.black_pawns + self.black_bishops + self.black_knights + self.black_rooks + self.black_queens + [self.black_king]
+
+    def get_square(self, square):
+        for pawn in self.pawns:
+            if pawn.column + pawn.row == square:
+                return pawn
+        for knight in self.knights:
+            if knight.column + knight.row == square:
+                return knight
+        for bishop in self.bishops:
+            if bishop.column + bishop.row == square:
+                return bishop
+        for rook in self.rooks:
+            if rook.column + rook.row == square:
+                return rook
+        for queen in self.queens:
+            if queen.column + queen.row == square:
+                return queen
+        for king in self.kings:
+            if king.column + king.row == square:
+                return king
+        
+        return 'empty'
+
+
+
 class Piece:
     def __init__(self, color, row, column):
         self.color = color
         self.row = row
         self.column = column
         self.possible_moves = []
+        self.filtered_moves = []
 
 class Knight(Piece):
     def __init__(self, color, row, column):
         super().__init__(color, row, column)
+        self.type = 'knight'
     
     def get_possible_moves(self):
+        self.possible_moves = []
         if self.row != '1':
             if self.column not in ['G', 'H']:
                 self.possible_moves.append(next_column(next_column(self.column)) + previous_row(self.row))
             if self.column not in ['A', 'B']:
                 self.possible_moves.append(previous_column(previous_column(self.column)) + previous_row(self.row))
-        if self.row != '2':
+        if self.row not in ['1', '2']:
             if self.column != 'H':
                 self.possible_moves.append(next_column(self.column) + previous_row(previous_row(self.row)))
             if self.column != 'A':
                 self.possible_moves.append(previous_column(self.column) + previous_row(previous_row(self.row)))
-        if self.row != '7':
+        if self.row != '8':
             if self.column != 'H':
                 self.possible_moves.append(next_column(self.column) + next_row(next_row(self.row)))
             if self.column != 'A':
                 self.possible_moves.append(previous_column(self.column) + next_row(next_row(self.row)))
-        if self.row != '8':
+        if self.row not in ['7', '8']:
             if self.column not in ['G', 'H']:
                 self.possible_moves.append(next_column(next_column(self.column)) + next_row(self.row))
             if self.column not in ['A', 'B']:
@@ -84,11 +129,14 @@ class Knight(Piece):
         return self.possible_moves
 
 
+
 class Bishop(Piece):
     def __init__(self, color, row, column):
         super().__init__(color, row, column)
+        self.type = 'bishop'
     
     def get_possible_moves(self):
+        self.possible_moves = []
         self.possible_moves = [square for square in diagonals(self.column, self.row) if square != self.column + self.row]
 
         return self.possible_moves
@@ -97,8 +145,10 @@ class Bishop(Piece):
 class Rook(Piece):
     def __init__(self, color, row, column):
         super().__init__(color, row, column)
+        self.type = 'rook'
     
     def get_possible_moves(self):
+        self.possible_moves = []
         for col in COLUMNS:
             if col != self.column:
                 self.possible_moves.append(col + self.row)
@@ -112,8 +162,10 @@ class Rook(Piece):
 class Queen(Piece):
     def __init__(self, color, row, column):
         super().__init__(color, row, column)
+        self.type = 'queen'
     
     def get_possible_moves(self):
+        self.possible_moves = []
         for col in COLUMNS:
             if col != self.column:
                 self.possible_moves.append(col + self.row)
@@ -124,12 +176,13 @@ class Queen(Piece):
         return self.possible_moves
         
 
-
 class King(Piece):
     def __init__(self, color, row, column):
         super().__init__(color, row, column)
+        self.type = 'king'
     
     def get_possible_moves(self):
+        self.possible_moves = []
         if self.row != '1':
             self.possible_moves.append(self.column + previous_row(self.row))
             if self.column != 'A':
@@ -153,8 +206,10 @@ class King(Piece):
 class Pawn(Piece):
     def __init__(self, color, row, column):
         super().__init__(color, row, column)
+        self.type = 'pawn'
     
     def get_possible_moves(self):
+        self.possible_moves = []
         if self.color == 'white':
             self.possible_moves.append(self.column + next_row(self.row))
             if self.column != 'A':
--- API/utils.py
+++ API/utils.py
@@ -40,3 +40,124 @@ def diagonals(column, row):
         j = previous_row(j)
         diag.append(i + j)
     return diag
+
+def available_diagonal(column, row, board):
+    available_diag = []
+    i = column
+    j = row
+    found_obstacle = False
+    while i != 'H' and j != '8' and not found_obstacle:
+        i = next_column(i)
+        j = next_row(j)
+        if board.get_square(i + j) == 'empty':
+            available_diag.append(i + j)
+        else:
+            found_obstacle = True
+    i = column
+    j = row
+    found_obstacle = False
+    while i != 'H' and j != '1' and not found_obstacle:
+        i = next_column(i)
+        j = previous_row(j)
+        if board.get_square(i + j) == 'empty':
+            available_diag.append(i + j)
+        else:
+            found_obstacle = True
+    i = column
+    j = row
+    found_obstacle = False
+    while i != 'A' and j != '8' and not found_obstacle:
+        i = previous_column(i)
+        j = next_row(j)
+        if board.get_square(i + j) == 'empty':
+            available_diag.append(i + j)
+        else:
+            found_obstacle = True
+    i = column
+    j = row
+    found_obstacle = False
+    while i != 'A' and j != '1' and not found_obstacle:
+        i = previous_column(i)
+        j = previous_row(j)
+        if board.get_square(i + j) == 'empty':
+            available_diag.append(i + j)
+        else:
+            found_obstacle = True
+    return available_diag
+
+
+def available_line(column, row, board):
+    available = []
+    i = column
+    j = row
+    found_obstacle = False
+    while i != 'A' and not found_obstacle:
+        i = previous_column(i)
+        if board.get_square(i + j) == 'empty':
+            available.append(i + j)
+        else:
+            found_obstacle = True
+    i = column
+    found_obstacle = False
+    while i != 'H' and not found_obstacle:
+        i = next_column(i)
+        if board.get_square(i + j) == 'empty':
+            available.append(i + j)
+        else:
+            found_obstacle = True
+    i = column
+    found_obstacle = False
+    while j != '1' and not found_obstacle:
+        j = previous_row(j)
+        if board.get_square(i + j) == 'empty':
+            available.append(i + j)
+        else:
+            found_obstacle = True
+    j = row
+    found_obstacle = False
+    while j != '8' and not found_obstacle:
+        j = next_row(j)
+        if board.get_square(i + j) == 'empty':
+            available.append(i + j)
+        else:
+            found_obstacle = True
+    
+    return available
+
+
+def filter_moves(piece, board):
+    possible_moves = piece.get_possible_moves()
+    filtered_moves = []
+    for move in possible_moves:
+        square = board.get_square(move)
+        if square == 'empty' or square.color != piece.color:
+            if piece.type in ['bishop', 'queen']:
+                if move in available_diagonal(piece.column, piece.row, board):
+                    filtered_moves.append(move)
+            elif piece.type in ['rook', 'queen']:
+                if move in available_line(piece.column, piece.row, board):
+                    filtered_moves.append(move)
+            elif piece.type == 'pawn':
+                if piece.color == 'white':
+                    if move[1] == '4' and piece.row == '2':
+                        if board.get_square(piece.column + '3') == 'empty':
+                            filtered_moves.append(move)
+                    else:
+                        if move[0] == piece.column and square == 'empty':
+                            filtered_moves.append(move)
+                else:
+                    if move[1] == '5' and piece.row == '7':
+                        if board.get_square(piece.column + '6') == 'empty':
+                            filtered_moves.append(move)
+                    else:
+                        filtered_moves.append(move)
+            elif piece.type == 'knight':
+                filtered_moves.append(move)
+            else:
+                filtered_moves.append(move)
+        if piece.type == 'pawn':                  
+            if move[0] != piece.column and square != 'empty' and square.color != piece.color:
+                filtered_moves.append(move)
+    
+    return filtered_moves
+            
